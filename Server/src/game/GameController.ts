00b379256af43db14ec9113eab8c48ac4b83147d
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2022-05-28 15:30:25 -0400

    region start

--- Server/src/game/GameController.ts
+++ Server/src/game/GameController.ts
@@ -1,13 +1,28 @@
-import { ClickMessage, UpgradeMessage } from "./model";
+import { ClickMessage, Region, UpgradeMessage } from "./model";
 
 export default class GameController {
+    static readonly DELAY_INTERVAL_FOR_LOOP_MILLIS = 1000;
+    static readonly MAXPOP = 5000;
+    static readonly MAP_WIDTH = 35;
+    static readonly MAP_HEIGHT = 5;
+
+    regions: Region[] = [];
 
     constructor() {
-        //TODO setup repeating task
+        const numOfRegions = GameController.MAP_HEIGHT * GameController.MAP_WIDTH;
+        
+        for(let r = 0; r < numOfRegions; r++) {
+            let region: Region = new Region();
+            region.id = r;
+            region.maxPopulation = this.getRandomInRange(100, GameController.MAXPOP);
+            region.infectedNumber = Math.floor(Math.random()) * GameController.MAXPOP;
+        }
+
+        setInterval(this.onRepeatingTask, GameController.DELAY_INTERVAL_FOR_LOOP_MILLIS);
     }
 
     public onClick(event: ClickMessage) {
-        //TODO
+        
     }
 
     public onUpgrade(event: UpgradeMessage) {
@@ -16,5 +31,16 @@ export default class GameController {
 
     public onRepeatingTask() {
         //TODO
+    }
+
+    private getRandomInRange(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    private isAdjacent(id: number, other: number): boolean {
+                //checks if adjecent horizontally
+                //note, if a grid is 5 in width, index 4 and 5 are not adjecent horizontally because next row
+        return (Math.abs(id - other) == 1 && !(id % GameController.MAP_WIDTH == 0 || other % GameController.MAP_WIDTH == 0)) ||
+                (Math.abs(id - other) == GameController.MAP_WIDTH);
     }
 }
