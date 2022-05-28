fa329d6d01c160a7a230dc7361960e2f6eddbf31
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-05-28 15:29:03 -0400

    added upgrade stuff

--- Server/src/game/GameController.ts
+++ Server/src/game/GameController.ts
@@ -1,6 +1,10 @@
 import { ClickMessage, UpgradeMessage } from "./model";
+import { Upgrade, upgrades } from "./upgrade";
 
 export default class GameController {
+
+    private clickerUpgrades: Upgrade[] = [];
+    private passiveUpgrades: Upgrade[] = [];
 
     constructor() {
         //TODO setup repeating task
@@ -8,13 +12,42 @@ export default class GameController {
 
     public onClick(event: ClickMessage) {
         //TODO
+        for(let upgrade of this.clickerUpgrades){
+
+            upgrade.execute();
+
+        }
+
     }
 
     public onUpgrade(event: UpgradeMessage) {
         //TODO
+        let upgrade = upgrades.get(event.upgrade);
+        if(upgrade === undefined){
+
+            throw new Error("Upgrade not found");
+
+        }
+        
+        if(upgrade.upgradeType === 'CLICKER_UPGRADE'){
+
+            this.clickerUpgrades.push(upgrade);
+
+        }else{
+
+            this.passiveUpgrades.push(upgrade);
+
+        }
+
     }
 
     public onRepeatingTask() {
         //TODO
+        for(let upgrade of this.passiveUpgrades){
+
+            upgrade.execute();
+
+        }
+
     }
 }
