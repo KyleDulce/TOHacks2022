53a1df39801f1086af675cfe13dc9881476d93d1
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2022-05-28 22:50:08 -0400

    team select

--- Client/src/lib/Clicker.svelte
+++ Client/src/lib/Clicker.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { WebSocket, GameState, Region } from "../stores";
+  import { WebSocket, GameState, Region, Team } from "../stores";
 
   function click() {
-    $WebSocket.emit("click", { region: $Region, team: 0 });
+    $WebSocket.emit("click", { region: $Region, team: $Team });
   }
-
   $: Selected = $GameState?.regions[$Region >= 0 ? $Region : 0];
 </script>
 
@@ -12,8 +11,9 @@
   <h1>Stats</h1>
 
   <div class="stats">
+    <h3>Team: {$Team === 0 ? "Humanity" : "Covid"}</h3>
     {#if $Region !== -1}
-      <h2>Region Name: {Selected?.name}</h2>
+      <h3>Region Name: {Selected?.name}</h3>
       <h2>
         Region infections: {Selected?.infectedNumber}
       </h2>
@@ -25,12 +25,38 @@
     {/if}
   </div>
 
-  <button on:click={click} disabled={$Region === -1}>
+  <button class="clicker-btn" on:click={click} disabled={$Region === -1}>
     <i class="fa-solid fa-earth-americas globe" />
   </button>
+
+  <h2 class="t">Choose Team:</h2>
+  <div class="teams">
+    <button
+      on:click={() => {
+        Team.set(0);
+      }}>Humanity</button
+    >
+    <button
+      on:click={() => {
+        Team.set(1);
+      }}>Covid</button
+    >
+  </div>
 </div>
 
 <style>
+  .t {
+    margin-bottom: 0.4rem;
+  }
+  .teams {
+    display: flex;
+  }
+
+  .teams button {
+    width: 100%;
+    margin: 0rem 0.2rem;
+  }
+
   h1 {
     margin: 0;
   }
@@ -43,8 +69,39 @@
     margin: 0;
   }
 
-  button {
+  h3 {
+    margin: 0;
+  }
+
+  .clicker-btn {
     border-radius: 100%;
+    background-color: var(--special-color);
+    transform: translateY(-20px);
+    box-shadow: var(--special-color-lip) 0px 20px 0px;
+
+    cursor: pointer;
+    /* box-shadow: 0px 0px 10px 2px var(--accent-color-shadow); */
+    transition: all 50ms;
+  }
+
+  .clicker-btn:disabled {
+    /* color: var(--text-color-disabled); */
+    box-shadow: none;
+    transform: translateY(-20px);
+    background-color: var(--special-color-disabled);
+    cursor: default !important;
+  }
+
+  .clicker-btn:not(:disabled):active {
+    /* color: var(--text-color-active); */
+    transform: translateY(-5px);
+    box-shadow: var(--special-color-lip) 0px 5px 0px;
+    /* background-color: var(--special-color-active); */
+  }
+
+  .clicker-btn:not(:disabled):not(:active):hover {
+    transform: translateY(-20px);
+    box-shadow: var(--special-color-lip) 0px 20px 0px;
   }
 
   .globe {
--- Client/src/lib/Upgrades.svelte
+++ Client/src/lib/Upgrades.svelte
@@ -1,17 +1,4 @@
 <script lang="ts">
-  import Grid from "./Grid.svelte";
-  import GridCell from "./GridCell.svelte";
-
-  let clicked = "0x0";
-
-  let board = [];
-  for (let _ of Array(5).fill(0)) {
-    let row = [];
-    for (let _ of Array(35).fill(0)) {
-      row.push(false);
-    }
-    board.push(row);
-  }
 </script>
 
 <div class="container">
