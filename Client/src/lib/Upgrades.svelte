6fd199b3a20bb00e12c5928009cce1f0653777b6
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-05-28 23:05:20 -0400

    upgrades loop thing

--- Client/src/lib/Upgrades.svelte
+++ Client/src/lib/Upgrades.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+
+  import Grid from "./Grid.svelte";
+  import GridCell from "./GridCell.svelte";
+  import {GameState, Team} from "../stores";
+
+  $: upgrades = $Team === 0 ? $GameState?.whoUpgrades : $GameState?.infectedUpgrades;
+
+  $: console.log(upgrades);
+
 </script>
 
 <div class="container">
   <h2>Upgrades</h2>
+  {#each upgrades || [] as upgrade }
+    <div class="container">
+      {upgrade}
+    </div>
+  {/each}
 </div>
 
 <style>
